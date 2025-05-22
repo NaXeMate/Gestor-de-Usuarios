7453e9470905f5cbdc5e13ec94cc90b36c94b407
NaXeMate <181981244+MateoNX@users.noreply.github.com>
2025-05-22 12:46:41 +0200

    Fully configured class Usuario

--- GestorUsuarios/src/com/liceolapaz/daw/mqt/Usuario.java
+++ GestorUsuarios/src/com/liceolapaz/daw/mqt/Usuario.java
@@ -6,4 +6,38 @@ public class Usuario {
     private String firstName;
     private String lastName;
     private String avatar;
+
+    public Usuario(int id, String email, String firstName, String lastName, String avatar) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", avatar=" + avatar + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
 }
