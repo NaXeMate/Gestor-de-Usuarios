7be425fd5b31eabfbd2b51c6f156dddc1bed816f
NaXeMate <181981244+MateoNX@users.noreply.github.com>
2025-05-22 12:35:00 +0200

    First commit

---
+++ GestorUsuarios/src/com/liceolapaz/daw/mqt/Principal.java
@@ -0,0 +1,7 @@
+package com.liceolapaz.daw.mqt;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ GestorUsuarios/src/com/liceolapaz/daw/mqt/Usuario.java
@@ -0,0 +1,9 @@
+package com.liceolapaz.daw.mqt;
+
+public class Usuario {
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+}
